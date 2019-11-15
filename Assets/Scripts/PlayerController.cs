3221b233992e890cf7e14c235daabd3a741a917a
Matan Maron <matanmaron@gmail.com>
2019-11-15 13:17:44 +0200

    first commit
    basic player controll. jump. ground.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //publics
+    [SerializeField] private CharacterController2D _characterController2D;
+    [SerializeField] private float runSpeed = 40;
+
+    //privets
+    private float moveHorizontal = 0;
+    private bool isJump = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //player control
+        moveHorizontal = Input.GetAxisRaw("Horizontal") * runSpeed;
+        isJump = Input.GetButtonDown("Jump");
+    }
+
+    private void LateUpdate()
+    {
+       //camera movement
+    }
+
+    private void FixedUpdate()
+    {
+        //Physics and collisions
+        PlayerMovmentHandler();
+    }
+
+    private void PlayerMovmentHandler()
+    {
+        _characterController2D.Move(moveHorizontal * Time.fixedDeltaTime, false, isJump);
+    }
+}
